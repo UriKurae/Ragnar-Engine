53e12890bb634147d0d47ba27ad56b7d3aa36469
Oriol Bernal Martinez <youtfrost@gmail.com>
2021-09-20 09:18:00 +0200

    Initial Commit
    This commit includes the Physics II code to work on it.
    
    Co-Authored-By: Lucas Pérez <lucaspg14@gmail.com>

---
+++ ModuleSceneIntro.cpp
@@ -0,0 +1,46 @@
+#include "Globals.h"
+#include "Application.h"
+#include "ModuleSceneIntro.h"
+#include "Primitive.h"
+
+ModuleSceneIntro::ModuleSceneIntro(Application* app, bool start_enabled) : Module(app, start_enabled)
+{
+}
+
+ModuleSceneIntro::~ModuleSceneIntro()
+{}
+
+// Load assets
+bool ModuleSceneIntro::Start()
+{
+	LOG("Loading Intro assets");
+	bool ret = true;
+
+	// TODO 2: Place the camera one unit up in Y and one unit to the right
+	// experiment with different camera placements, then use LookAt()
+	// to make it look at the center
+
+	return ret;
+}
+
+// Load assets
+bool ModuleSceneIntro::CleanUp()
+{
+	LOG("Unloading Intro scene");
+
+	return true;
+}
+
+// Update: draw background
+update_status ModuleSceneIntro::Update()
+{
+	// TODO 1: Create a Plane primitive. This uses the plane formula
+	// so you have to express the normal of the plane to create 
+	// a plane centered around 0,0. Make that it draw the axis for reference
+
+	// TODO 6: Draw a sphere of 0.5f radius around the center
+	// Draw somewhere else a cube and a cylinder in wireframe
+
+	return UPDATE_CONTINUE;
+}
+
