4ffc7ba2ea5a99652a8453b2d4105f8198121a3c
UriKurae <youtfrost@gmail.com>
2021-10-11 13:24:16 +0200

    Geometry done
    Updated the loading of a mesh.
    
    Co-Authored-By: Lucas Pérez <lucaspg14@gmail.com>

--- Engine/Source/Mesh.cpp
+++ Engine/Source/Mesh.cpp
@@ -1,6 +1,7 @@
 #include "Mesh.h"
 
 #include "glew/include/GL/glew.h"
+#include "MathGeoLib/src/MathGeoLib.h"
 
 Mesh::Mesh(std::vector<Vertex> vert, std::vector<unsigned int> ind, std::vector<Texture> text) : vertices(vert), indices(ind), textures(text), vao(0), vbo(0), ebo(0)
 {
@@ -11,6 +12,36 @@ void Mesh::Draw()
 {
 	glBindVertexArray(vao);
 	glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
+	/*glBegin(GL_LINES);
+	for (int i = 0; i < vertices.size(); ++i)
+	{
+		glVertex3f(vertices[i].position.x, vertices[i].position.y, vertices[i].position.z);
+		float3 vertexNorm;
+		vertexNorm.x = vertices[i].position.x + vertices[i].normal.x;
+		vertexNorm.y = vertices[i].position.y + vertices[i].normal.y;
+		vertexNorm.z = vertices[i].position.z + vertices[i].normal.z;
+		glVertex3f(vertexNorm.x, vertexNorm.y, vertexNorm.z);
+	}
+	glEnd();*/
+
+	/*glBegin(GL_LINES);
+	for (int i = 0; i < vertices.size(); i+=3)
+	{
+		float3 line1 = -(vertices[i].position - vertices[i+1].position);
+		float3 line2 = (vertices[i+1].position - vertices[i+2].position);
+		float3 normalVector = math::Cross(line1, line2);
+		normalVector.Normalize();
+
+		float3 center;
+		center.x = (vertices[i].position.x + vertices[i+1].position.x + vertices[i+2].position.x) / 3;
+		center.y = (vertices[i].position.y + vertices[i+1].position.y + vertices[i+2].position.y) / 3;
+		center.z = (vertices[i].position.z + vertices[i+1].position.z + vertices[i+2].position.z) / 3;
+
+		glVertex3f(center.x, center.y, center.z);
+		glVertex3f(center.x+normalVector.x, center.y + normalVector.y, center.z + normalVector.z);
+	}
+	glEnd();*/
+
 	glBindVertexArray(0);
 }
 
