3aab62b437fed4fe5365a21047c389cd04093b85
himar33 <himbrgo@iesvalldemossa.net>
2022-02-20 21:19:28 +0100

    Input action model define

--- Engine/Source/InputActionComponent.cpp
+++ Engine/Source/InputActionComponent.cpp
@@ -1,5 +1,6 @@
 
 #include "InputActionComponent.h"
+#include "IconsFontAwesome5.h"
 
 InputActionComponent::InputActionComponent(GameObject* own, TransformComponent* trans)
 {
@@ -9,4 +10,29 @@ InputActionComponent::InputActionComponent(GameObject* own, TransformComponent* 
 
 InputActionComponent::~InputActionComponent()
 {
+}
+
+void InputActionComponent::OnEditor()
+{
+	ImGui::PushID(this);
+	if (ImGui::CollapsingHeader(ICON_FA_GAMEPAD" Player Input"))
+	{
+
+	}
+	ImGui::PopID();
+}
+
+bool InputActionComponent::Update(float dt)
+{
+	return false;
+}
+
+bool InputActionComponent::OnLoad(JsonParsing& node)
+{
+	return false;
+}
+
+bool InputActionComponent::OnSave(JsonParsing& node, JSON_Array* array)
+{
+	return false;
 }
--- Engine/Source/InputActionComponent.h
+++ Engine/Source/InputActionComponent.h
@@ -1,21 +1,11 @@
 #pragma once
 
 #include "Component.h"
+
+#include <string>
 #include <vector>
 
 class TransformComponent;
-
-class Bindings
-{
-public:
-	Bindings();
-	~Bindings();
-
-private:
-
-
-
-};
 
 class Actions
 {
@@ -24,17 +14,36 @@ public:
 	~Actions();
 
 private:
+	std::string name;
+	//std::vector<Bindings> bindings;
+};
 
-	std::vector<Bindings> bindings;
+class ActionMaps
+{
+public:
+	ActionMaps();
+	~ActionMaps();
 
+private:
+	std::string name;
+	std::vector<Actions> actions;
 };
 
 class InputActionComponent : Component
 {
 public:
+
 	InputActionComponent(GameObject* own, TransformComponent* trans);
 	~InputActionComponent();
 
+	void OnEditor() override;
+	bool Update(float dt) override;
+
+	bool OnLoad(JsonParsing& node) override;
+	bool OnSave(JsonParsing& node, JSON_Array* array) override;
+
 private:
+
+
 
 };
