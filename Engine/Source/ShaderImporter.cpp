b1ef6de0870f39666b1d94368db90e464915caea
David González <61042946+MagiX7@users.noreply.github.com>
2022-02-25 17:56:09 +0100

    Update ShaderImporter.cpp

--- Engine/Source/ShaderImporter.cpp
+++ Engine/Source/ShaderImporter.cpp
@@ -13,8 +13,6 @@ uint ShaderImporter::SaveShader(std::string& assets, std::string& source)
 {
 	std::string lib;
 	uint uid = ResourceManager::GetInstance()->CreateResource(ResourceType::SHADER, assets, lib);
-	std::shared_ptr<Shader> sh = std::static_pointer_cast<Shader>(ResourceManager::GetInstance()->GetResource(uid));
-
 	CopyFileA(assets.c_str(), lib.c_str(), false);
 
 	return uid;
