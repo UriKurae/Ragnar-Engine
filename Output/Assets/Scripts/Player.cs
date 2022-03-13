9204c89d19f5f55b8c6ea289b9243565a9defbc4
Makinilla-maker <valverdeoriol@gmail.com>
2022-03-13 19:09:08 +0100

    Ability prototipe
    Co-Authored-By: Ignasi Pardo Carbó <60847464+KuronoaScarlet@users.noreply.github.com>

--- Output/Assets/Scripts/Player.cs
+++ Output/Assets/Scripts/Player.cs
@@ -6,6 +6,7 @@ public class Player : RagnarComponent
 {
 	public int velocity = 5;
 	public GameObject target = null;
+    public float force = 100;
 
     Rigidbody rb;
     MaterialComponent materialComponent;
@@ -73,7 +74,7 @@ public class Player : RagnarComponent
             rb.ApplyCentralForce(f);
         }
 
-        if (Input.GetKey(KeyCode.SPACE) == KeyState.KEY_DOWN)
+        if (Input.GetKey(KeyCode.F1) == KeyState.KEY_DOWN)
         {
             Vector3 pos = gameObject.transform.globalPosition;
             pos.y += 1;
@@ -82,10 +83,23 @@ public class Player : RagnarComponent
 
             Rigidbody bulletRb = bullet.CreateComponent<Rigidbody>();
             bulletRb.IgnoreCollision(gameObject, true);
-            bulletRb.ApplyCentralForce(gameObject.transform.forward * 1000);
+            Vector3 vectorDir = new Vector3(gameObject.transform.forward.x, 1, gameObject.transform.forward.z);
+            bulletRb.ApplyCentralForce(vectorDir.normalized * force);
 
-            AudioSource audio = gameObject.GetComponent<AudioSource>();
-            audio.PlayClip("Shot");
+            //AudioSource audio = gameObject.GetComponent<AudioSource>();
+            //audio.PlayClip("Shot");
+        }
+
+        if (Input.GetKey(KeyCode.F2) == KeyState.KEY_DOWN)
+        {
+            Vector3 pos = gameObject.transform.globalPosition;
+            pos.y += 1;
+            GameObject bullet = InternalCalls.Create3DObject("Knife", (int)PrimitiveType.CUBE, pos);
+            bullet.transform.scale = new Vector3(0.2f, 0.2f, 0.2f);
+
+            Rigidbody bulletRb = bullet.CreateComponent<Rigidbody>();
+            bulletRb.IgnoreCollision(gameObject, true);
+            bulletRb.ApplyCentralForce(gameObject.transform.forward * 1000);
         }
     }
 }
