9fbbe514e536196e7ae05091753d73e4fe8a2543
Osvak <oscarcanales2001@gmail.com>
2022-03-22 21:40:45 +0100

    Pause Menu + Skills UI
    Co-Authored-By: unaidiaz <61042480+unaidiaz@users.noreply.github.com>

--- Output/Assets/Scripts/Abilities.cs
+++ Output/Assets/Scripts/Abilities.cs
@@ -3,24 +3,27 @@ using RagnarEngine;
 
 public class Abilities : RagnarComponent
 {
-
+    GameObject SkillKnife;
+    GameObject SkillNade;
     public void Start()
     {
         Debug.Log("Starting Ability Manager");
+        SkillKnife = GameObject.Find("SkillKnife");
+        SkillNade = GameObject.Find("SkillNade");
     }
 
     public void Update()
     {
         ///////// ABILITIES /////////
         // Rock Throw
-        if (Input.GetKey(KeyCode.F1) == KeyState.KEY_DOWN && GameObject.Find("Rock") == null)
+        if ((Input.GetKey(KeyCode.F1) == KeyState.KEY_DOWN || SkillNade.GetComponent<UIButton>().GetButtonState() == 3) && GameObject.Find("Rock") == null)
         {
 
             InternalCalls.InstancePrefab("Assets/Prefabs/Rock.rgprefab");
         }
 
         // Knife Throw
-        if (Input.GetKey(KeyCode.F2) == KeyState.KEY_DOWN && GameObject.Find("Knife") == null)
+        if ((Input.GetKey(KeyCode.F2) == KeyState.KEY_DOWN || SkillKnife.GetComponent<UIButton>().GetButtonState() == 3) && GameObject.Find("Knife") == null)
         {
             InternalCalls.InstancePrefab("Assets/Prefabs/Knife.rgprefab");
         }
--- Output/Assets/Scripts/pauseMenuButton.cs
+++ Output/Assets/Scripts/pauseMenuButton.cs
@@ -6,40 +6,192 @@ public class pauseMenuButton : RagnarComponent
 	Vector3 pos;
 	bool isSowing = false;
 	bool firstTime = true;
-	
+	bool isFirstS = true;
+	GameObject Image;
+	GameObject Resume;
+	GameObject MainM;
+	GameObject Opt;
+	GameObject Rect;
 	public void Start()
     {
-		
-		
+
 	}
 	
 	public void Update()
 	{
 		if (firstTime)
 		{
-			pos = gameObject.GetComponent<Transform2D>().position2D;
-			pos.Set(0.0f, 1000.0f, 0.0f);
-			gameObject.GetComponent<Transform2D>().position2D = pos;
+			Image = GameObject.Find("BackgroundImage");
+			Rect = GameObject.Find("UIRectangle");
+			Resume = GameObject.Find("ButtonResume");
+			MainM = GameObject.Find("ButtonMainMenu");
+			Opt = GameObject.Find("ButtonOptions");
+
+			pos = new Vector3(0.0f, 0.0f, 0.0f);
+
+			ImageHide();
+			RectangleHide();
+			ResumeButtonHide();
+			MainMenuButtonHide();
+			OptionsButtonHide();
+
 			firstTime = false;
 		}
 
-		if (Input.GetKey(KeyCode.A) == KeyState.KEY_DOWN)
+		if(isSowing)
+        {
+			ImageShow();
+			RectangleShow();
+			ResumeButtonShow();
+			MainMenuButtonShow();
+			OptionsButtonShow();
+			if(isSowing == false)
+            {
+				ImageHide();
+				RectangleHide();
+				ResumeButtonHide();
+				MainMenuButtonHide();
+				OptionsButtonHide();
+			}
+		}
+
+		if (Input.GetKey(KeyCode.P) == KeyState.KEY_DOWN)
 		{
 			if (isSowing)
 			{
 				isSowing = false;
-				pos.Set(0.0f, 1000.0f, 0.0f);
-				gameObject.GetComponent<Transform2D>().position2D = pos;
+
+				ImageShow();
+				RectangleShow();
+				ResumeButtonShow();
+				MainMenuButtonShow();
+				OptionsButtonShow();
+
 			}
 			else
 			{
-				pos.Set(0.0f, 100.0f, 0.0f);
-            
-				gameObject.GetComponent<Transform2D>().position2D = pos;
+				ImageHide();
+				RectangleHide();
+				ResumeButtonHide();
+				MainMenuButtonHide();
+				OptionsButtonHide();
 				isSowing = true;
 				
 			}
 		}
 	}
 
+	void ImageShow()
+    {
+		pos.Set(0.0f, 0.0f, 84.0f);
+		Image.GetComponent<Transform2D>().position2D = pos;
+		Image.GetComponent<Transform2D>().SetSize(InternalCalls.GetRegionGame());
+	}
+
+	void ImageHide()
+    {
+		pos.Set(0.0f, 1000.0f, 84.0f);
+		Image.GetComponent<Transform2D>().position2D = pos;
+	}
+
+	void ResumeButtonShow()
+	{
+		pos.Set(-363.0f, 30.0f, 90.0f);
+		Resume.GetComponent<Transform2D>().position2D = pos;
+
+		int a = Resume.GetComponent<UIButton>().GetButtonState();
+		switch (a)
+		{
+			case 0:
+				// disabled Mode
+				break;
+			case 1:
+				isFirstS = true;
+				// normal Mode
+				break;
+			case 2:
+				// focused mode
+				if (isFirstS)
+				{
+					isFirstS = false;
+
+					//poner sonido
+				}
+
+				break;
+			case 3:
+				// pressed mode
+				isSowing = false;
+				//Quitar menu de pausa
+				break;
+		}
+	}
+	void ResumeButtonHide()
+	{
+		pos.Set(0.0f, 1000.0f, 90.0f);
+		Resume.GetComponent<Transform2D>().position2D = pos;
+	}
+
+	void MainMenuButtonShow()
+	{
+		pos.Set(-363.0f, -28.0f, 90.0f);
+		MainM.GetComponent<Transform2D>().position2D = pos;
+
+		int a = MainM.GetComponent<UIButton>().GetButtonState();
+		switch (a)
+		{
+			case 0:
+				// disabled Mode
+				break;
+			case 1:
+				isFirstS = true;
+				// normal Mode
+				break;
+			case 2:
+				// focused mode
+				if (isFirstS)
+				{
+					isFirstS = false;
+
+					//poner sonido
+				}
+
+				break;
+			case 3:
+				// pressed mode
+				SceneManager.LoadScene("MainMenu");
+				//cambiar de escena
+				break;
+		}
+	}
+	void MainMenuButtonHide()
+	{
+		pos.Set(0.0f, 1000.0f, 90.0f);
+		MainM.GetComponent<Transform2D>().position2D = pos;
+	}
+
+
+	void OptionsButtonShow()
+	{
+		pos.Set(-356.5f, -0.0f, 90.0f);
+		Opt.GetComponent<Transform2D>().position2D = pos;
+
+		// Options button logic
+	}
+	void OptionsButtonHide()
+	{
+		pos.Set(0.0f, 1000.0f, 90.0f);
+		Opt.GetComponent<Transform2D>().position2D = pos;
+	}
+	void RectangleShow()
+	{
+		pos.Set(-326.0f, 7.5f, 88.2f);
+		Rect.GetComponent<Transform2D>().position2D = pos;
+	}
+	void RectangleHide()
+	{
+		pos.Set(0.0f, 1000.0f, 88.2f);
+		Rect.GetComponent<Transform2D>().position2D = pos;
+	}
 }
+
