407d1d6f34d9ca0f51fff672d13c31b8fb562996
Makinilla-maker <valverdeoriol@gmail.com>
2022-04-05 16:21:43 +0200

    camouflage implemented

--- Output/Assets/Scripts/Camouflage.cs
+++ Output/Assets/Scripts/Camouflage.cs
@@ -4,17 +4,35 @@ using RagnarEngine;
 public class Camouflage : RagnarComponent
 {
 	public GameObject player;
+	float time1 = 5f;
 	public void Start()
 	{
 		player = GameObject.Find("Player_2");
 	}
 	public void Update()
 	{
-		SetMaterialTransparent();
+		SetMaterialTransparent("Assets/Resources/UI/transparent_tex.png");
+		if(Timer())
+        {
+			SetMaterialTransparent("Assets/Resources/CharacterTex/char_chani_albedo.png");
+			InternalCalls.Destroy(gameObject);
+		}
 	}
-	private void SetMaterialTransparent()
+	private void SetMaterialTransparent(string path)
     {
-		player.GetComponent<Material>().SetTexturePath("Assets/Resources/UI/transparent_tex.png");
+		player.GetComponent<Material>().SetTexturePath(path);
     }
+	public bool Timer()//moltes gracies Isaac
+	{
+		if (time1 > 0)
+		{
+			time1 -= Time.deltaTime;
+			return false;
+		}
+		else
+		{
+			return true;
+		}
+	}
 
 }
