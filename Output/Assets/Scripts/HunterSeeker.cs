40bd85cd2642945ef5085f444efbe1583eaf2110
isaac553876299 <60979696+isaac553876299@users.noreply.github.com>
2022-04-20 09:43:36 +0200

    99% finished hunter seeker

--- Output/Assets/Scripts/HunterSeeker.cs
+++ Output/Assets/Scripts/HunterSeeker.cs
@@ -6,32 +6,38 @@ public class HunterSeeker : RagnarComponent
 	NavAgent agent;
 	public GameObject[] enemies;
 	Rigidbody rb;
+	GameObject area;
 
 	public void Start()
 	{
 		agent = gameObject.GetComponent<NavAgent>();
 		enemies = GameObject.FindGameObjectsWithTag("Enemies");
-		GameObject player = GameObject.Find("Player");
+		GameObject player = GameObject.Find("Player_2");
 		player.GetComponent<Player>().SetControled(false);
 		Vector3 pos = player.transform.globalPosition + new Vector3(0, 1, 0);
 		rb = gameObject.GetComponent<Rigidbody>();
 		rb.SetBodyPosition(pos);
 		rb.IgnoreCollision(player, true);
+		InternalCalls.InstancePrefab("Backstab Area");
+		area = GameObject.Find("Backstab Area");
+		gameObject.AddChild(area);
+		area.transform.localPosition = new Vector3(0, area.transform.localPosition.y, 0);
 	}
 	public void Update()
 	{
 		if (Input.GetMouseClick(MouseButton.LEFT) == KeyState.KEY_UP)
 		{
-			if (agent.CalculatePath(agent.hitPosition).Length > 0)
-			{ }
+			agent.CalculatePath(agent.hitPosition);
 		}
+		agent.MovePath();
 		if (Input.GetMouseClick(MouseButton.RIGHT) == KeyState.KEY_UP)
 		{
 			if (Attack())
 			{
-				GameObject player = GameObject.Find("Player");
+				GameObject player = GameObject.Find("Player_2");
 				player.GetComponent<Player>().SetControled(true);
-
+				gameObject.EraseChild(area);
+				//InternalCalls.Destroy(area);
 				InternalCalls.Destroy(gameObject);
 			}
 		}
@@ -41,7 +47,7 @@ public class HunterSeeker : RagnarComponent
 		for (int i = 0; i < enemies.Length; i++)
 		{
 			float distance = Vector3.Magnitude(gameObject.transform.globalPosition - enemies[i].transform.globalPosition);
-			if (distance <= 2)
+			if (distance <= 3.633)
 			{
 				enemies[i].GetComponent<EnemyInteractions>().pendingToDelete = true;
 				return true;
