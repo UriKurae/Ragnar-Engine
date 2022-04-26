a73e8b6ba80d576891efbb989f44dfbb02883aa9
Ignasi Pardo Carbó <60847464+KuronoaScarlet@users.noreply.github.com>
2022-04-26 21:00:27 +0200

    Abilities are now according to HUD

--- Output/Assets/Scripts/Level_2.cs
+++ Output/Assets/Scripts/Level_2.cs
@@ -25,29 +25,17 @@ public class Level_2 : RagnarComponent
         };
         characters[0].abilities[0] = new Abilities
         {
-            name = "Knife Throw",
-            prefabPath = "Knife",
-            transformY = 1.32f,
+            name = "BackStab",
+            prefabPath = "BackStab_2",
+            transformY = 0.2f,
             intensity = 4.0f,
-            constant = 1.232f,
-            linear = -0.172f,
+            constant = 0.1f,
+            linear = -0.574f,
             quadratic = 0f,
             charges = -1,
-            cooldown = 25f
-        }; // Throwing Knife
+            cooldown = 0f
+        }; // BackStab
         characters[0].abilities[1] = new Abilities
-        {
-            name = "Eagle Throw",
-            prefabPath = "Eagle",
-            transformY = 1.15f,
-            intensity = 4.0f,
-            constant = 1.129f,
-            linear = -0.188f,
-            quadratic = 0f,
-            charges = -1,
-            cooldown = 20f
-        }; // Rock/Eagle
-        characters[0].abilities[2] = new Abilities
         {
             name = "The Voice",
             prefabPath = "Voice",
@@ -59,18 +47,30 @@ public class Level_2 : RagnarComponent
             charges = -1,
             cooldown = 20f
         }; // The Voice
-        characters[0].abilities[3] = new Abilities
+        characters[0].abilities[2] = new Abilities
         {
-            name = "BackStab",
-            prefabPath = "BackStab_2",
-            transformY = 0.2f,
+            name = "Knife Throw",
+            prefabPath = "Knife",
+            transformY = 1.32f,
             intensity = 4.0f,
-            constant = 0.1f,
-            linear = -0.574f,
+            constant = 1.232f,
+            linear = -0.172f,
             quadratic = 0f,
             charges = -1,
-            cooldown = 0f
-        }; // BackStab
+            cooldown = 25f
+        }; // Throwing Knife
+        characters[0].abilities[3] = new Abilities
+        {
+            name = "Rock Throw",
+            prefabPath = "Rock",
+            transformY = 1.15f,
+            intensity = 4.0f,
+            constant = 1.129f,
+            linear = -0.188f,
+            quadratic = 0f,
+            charges = -1,
+            cooldown = 20f
+        }; // Rock/Eagle
 
         // Player 2
         characters[1] = new Characters
@@ -108,18 +108,6 @@ public class Level_2 : RagnarComponent
         }; // Camouflage
         characters[1].abilities[2] = new Abilities
         {
-            name = "Spice Grenade",
-            prefabPath = "Spice Grenade",
-            transformY = 1.32f,
-            intensity = 4.0f,
-            constant = 1.232f,
-            linear = -0.172f,
-            quadratic = 0f,
-            charges = -1,
-            cooldown = 0f
-        }; // Spice Bomb
-        characters[1].abilities[3] = new Abilities
-        {
             name = "Hunter Seeker",
             prefabPath = "HunterSeeker",
             transformY = 1.32f,
@@ -128,8 +116,20 @@ public class Level_2 : RagnarComponent
             linear = -0.172f,
             quadratic = 0f,
             charges = -1,
-            cooldown = 4f
+            cooldown = 40f
         }; // Hunter Seeker
+        characters[1].abilities[3] = new Abilities
+        {
+            name = "Spice Grenade",
+            prefabPath = "SpiceGrenade",
+            transformY = 1.32f,
+            intensity = 4.0f,
+            constant = 1.232f,
+            linear = -0.172f,
+            quadratic = 0f,
+            charges = -1,
+            cooldown = 0f
+        }; // Spice Bomb
 
         // Player 3
         characters[2] = new Characters
@@ -167,18 +167,6 @@ public class Level_2 : RagnarComponent
         }; // Stunner Shot
         characters[2].abilities[2] = new Abilities
         {
-            name = "Whistle",
-            prefabPath = "Whistle",
-            transformY = 1.12f,
-            intensity = 4.0f,
-            constant = 1.232f,
-            linear = -0.201f,
-            quadratic = 0f,
-            charges = -1,
-            cooldown = 6f
-        }; // Whistle
-        characters[2].abilities[3] = new Abilities
-        {
             name = "Trap",
             prefabPath = "Trap",
             transformY = 0.12f,
@@ -189,6 +177,18 @@ public class Level_2 : RagnarComponent
             charges = 1,
             cooldown = 0f
         }; // Trap
+        characters[2].abilities[3] = new Abilities
+        {
+            name = "Whistle",
+            prefabPath = "Whistle",
+            transformY = 1.12f,
+            intensity = 4.0f,
+            constant = 1.232f,
+            linear = -0.201f,
+            quadratic = 0f,
+            charges = -1,
+            cooldown = 6f
+        }; // Whistle
 
         // ENEMIES
         enemies = new Enemies[51];
