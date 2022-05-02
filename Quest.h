2fe5ad2382c4ff8466b755bafcd921d5495dddee
adrianam4 <61042878+adrianam4@users.noreply.github.com>
2022-05-02 17:20:15 +0200

    Quest System Created
    I've created the base Quest class and also the system files to manage them.

---
+++ Quest.h
@@ -0,0 +1,39 @@
+#pragma once
+#include "stdint.h"
+#include <string>
+
+typedef unsigned int uint;
+
+enum class QuestState
+{
+	ACTIVE = 0,
+	INACTIVE,
+	COMPLETED,
+};
+
+enum class QuestType
+{
+	STORY_QUEST = 0,
+	SECONDARY_QUEST,
+};
+
+class Quest {
+public:
+	// Constructors and destructor
+	Quest(uint id, std::string name, std::string description, QuestState state, QuestType type);
+	~Quest();
+private:
+	// Vars
+	uint id;
+	std::string name;
+	std::string description;
+	QuestState state;
+	QuestType type;
+public:
+	void ChangeState(QuestState newState);
+	uint GetQuestId();
+	std::string GetQuestName();
+	std::string GetQuestDescription();
+	QuestState GetQuestState();
+	QuestType GetQuestType();
+};
