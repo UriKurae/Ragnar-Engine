184d1a653483562f3df9bc941e102f2e7ffe15b4
arnaubonada <61042890+arnaubonada@users.noreply.github.com>
2022-05-09 02:01:22 +0200

    first iteration particles eagle and hunter seeker

--- Output/Assets/Scripts/Eagle.cs
+++ Output/Assets/Scripts/Eagle.cs
@@ -9,8 +9,10 @@ public class Eagle : RagnarComponent
     public float cooldown = 0;
     public float soundRadius = 6f;
     Rigidbody goRB;
-
+    ParticleSystem leftParticles;
+    ParticleSystem rightParticles;
     NavAgent agent;
+    
     public void Start()
 	{
         gameObject.GetComponent<AudioSource>().PlayClip("WPN_EAGLEORDER");
@@ -23,6 +25,10 @@ public class Eagle : RagnarComponent
         goRB.IgnoreCollision(player, true);
         agent.CalculatePath(agent.hitPosition);
         agent.MovePath();
+        leftParticles = GameObject.Find("LeftWingParticles").GetComponent<ParticleSystem>();
+        rightParticles = GameObject.Find("RightWingParticles").GetComponent<ParticleSystem>();
+        leftParticles.Play();
+        rightParticles.Play();
     }
 	public void Update()
 	{
@@ -39,6 +45,8 @@ public class Eagle : RagnarComponent
             {
                 cooldown = 0f;
                 InternalCalls.Destroy(gameObject);
+                leftParticles.Pause();
+                rightParticles.Pause();
             }
         }
     }
