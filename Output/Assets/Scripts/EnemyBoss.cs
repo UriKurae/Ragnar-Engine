aabdd3f5df0a7cf4cdda9fc06ccd221549c08286
Alejandro Moreno <61042882+JandroK@users.noreply.github.com>
2022-05-19 10:06:51 +0200

    Optimization ScriptComponent out Frustrum Culling

--- Output/Assets/Scripts/EnemyBoss.cs
+++ Output/Assets/Scripts/EnemyBoss.cs
@@ -233,7 +233,7 @@ public class EnemyBoss : RagnarComponent
         Vector3 enemyForward = gameObject.transform.forward;
         Vector3 initPos = new Vector3(enemyPos.x + (enemyForward.x * offset.x * 0.6f), enemyPos.y + 0.1f, enemyPos.z + (enemyForward.z * offset.z * 0.6f));
 
-        index = RayCast.PerceptionCone(initPos, enemyForward, 60, 10, 8, players, players.Length, colliders, colliders.Length);
+        index = RayCast.PerceptionCone(initPos, enemyForward, 60, 10, 8, players, players.Length, "Collider");
         if (players[index].GetComponent<Player>().invisible || players[index].GetComponent<Player>().dead) return false;
         return (index == -1) ? false : true;
     }
